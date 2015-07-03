9bc2d793f116d8adff1fe663c121abcdf7bfd31a
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-07-03 19:49:22 -0300

    Step 4

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,14 +9,18 @@
  */
 var phonecatApp = angular.module('phonecatApp', []); //ng-app
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) { //ng-controller
-  //Establecemos los datos que vamos a pasar a la vista, el scope es bi-direccional
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
-      'snippet': 'Fast just got faster with Nexus S.'},
+      'snippet': 'Fast just got faster with Nexus S.',
+      'age': 1},
     {'name': 'Motorola XOOM™ with Wi-Fi',
-      'snippet': 'The Next, Next Generation tablet.'},
+      'snippet': 'The Next, Next Generation tablet.',
+      'age': 2},
     {'name': 'MOTOROLA XOOM™',
-      'snippet': 'The Next, Next Generation tablet.'}
+      'snippet': 'The Next, Next Generation tablet.',
+      'age': 3}
   ];
+
+  $scope.orderProp = 'age';
 });
