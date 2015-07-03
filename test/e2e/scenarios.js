660e266d88163001bc85fcd22fff6978c3a53409
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-07-03 19:43:10 -0300

    Step 3

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,29 @@
+/**
+ * Created by alejandrobarreiro on 3/7/15.
+ */
+
+describe('PhoneCat App', function() {
+
+  describe('Phone list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+
+    it('should filter the phone list as a user types into the search box', function() {
+
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
+
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
+  });
+});
