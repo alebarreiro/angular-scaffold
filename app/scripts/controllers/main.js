3012b2f34c23a05c7304b450de5c9785de7b0173
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-07-03 20:33:01 -0300

    Step 6

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,26 +1,13 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name gpsDeployPageApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the gpsDeployPageApp
- */
-var phonecatApp = angular.module('phonecatApp', []); //ng-app
+/* Controllers */
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-      'snippet': 'Fast just got faster with Nexus S.',
-      'age': 1},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-      'snippet': 'The Next, Next Generation tablet.',
-      'age': 2},
-    {'name': 'MOTOROLA XOOM™',
-      'snippet': 'The Next, Next Generation tablet.',
-      'age': 3}
-  ];
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
 
   $scope.orderProp = 'age';
-});
+}]);
