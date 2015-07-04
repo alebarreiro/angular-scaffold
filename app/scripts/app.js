9556ef560ab7cab197c26d8cef75c063af1cda2c
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-07-03 21:32:35 -0300

    Step 7: Routing & multiple views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,24 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name gpsDeployPageApp
- * @description
- * # gpsDeployPageApp
- *
- * Main module of the application.
- */
-angular
-  .module('gpsDeployPageApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'views/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'views/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
       });
-  });
+  }]);
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,17 +1,1 @@
-'use strict';
 
-/**
- * @ngdoc function
- * @name gpsDeployPageApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the gpsDeployPageApp
- */
-angular.module('gpsDeployPageApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
